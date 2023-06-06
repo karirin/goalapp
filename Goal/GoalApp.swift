e09e3c51550bac7e04ff46a48572c641df584412
karirin <karirin419@gmail.com>
2023-06-06 22:05:56 +0900

    Initial Commit

---
+++ Goal/GoalApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalApp.swift
+//  Goal
+//
+//  Created by hashimo ryoya on 2023/06/06.
+//
+
+import SwiftUI
+
+@main
+struct GoalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
