f5b4646f46877a9aac5afe0e9819d3244b4e4c37
karirin <karirin419@gmail.com>
2023-11-16 12:34:43 +0900

    修正

--- Goal/GoalApp.swift
+++ Goal/GoalApp.swift
@@ -12,27 +12,76 @@ import GoogleMobileAds
 import StoreKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
-      GADMobileAds.sharedInstance().start(completionHandler: nil)
-      if FirebaseApp.app() == nil {
-          FirebaseApp.configure()
-      }
-      // アプリ起動時に実行
-      Task {
-          await checkSubscriptionStatus()
-      }
-      return true
-  }
-    
-    func checkSubscriptionStatus() async {
-        let transactions = await Transaction.currentEntitlements
-        let isSubscribed = transactions.contains { $0.productType == .autoRenewable } // 修正された部分
-
-        DispatchQueue.main.async {
-            // AppStateの更新
-            self.appState.isBannerVisible = !isSubscribed
+    @StateObject var appState = AppState()
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
         }
+        DispatchQueue.global(qos: .background).async {
+            self.checkSubscription()
+        }
+        return true
+     }
+    
+    func checkSubscription() {
+        Task {
+            do {
+                let subscribed = try await self.isSubscribed()
+                DispatchQueue.main.async {
+                    self.appState.isBannerVisible = !subscribed
+                }
+            } catch {
+                print("サブスクリプションの確認中にエラー: \(error)")
+            }
+        }
+    }
+
+    func checkVerified<T>(_ result: VerificationResult<T>) throws -> T {
+        switch result {
+        case let .unverified(_, verificationError):
+            throw verificationError
+        case let .verified(safe):
+            return safe
+        }
+    }
+
+    func getSubscriptionRenewalState(groupID: String) async throws -> [StoreKit.Product.SubscriptionInfo.RenewalState] {
+      var results: [StoreKit.Product.SubscriptionInfo.RenewalState] = []
+      
+      let statuses = try await Product.SubscriptionInfo.status(for: groupID)
+      for status in statuses {
+        guard case .verified(let renewalInfo) = status.renewalInfo,
+              case .verified(let transaction) = status.transaction
+        else {
+          continue
+        }
+        results.append(status.state)
+      }
+      return results
+    }
+      
+    func isSubscribed() async throws -> Bool {
+        var subscriptionGroupIds: [String] = []
+        for await result in Transaction.currentEntitlements {
+            let transaction = try self.checkVerified(result)
+            guard let groupId = transaction.subscriptionGroupID else { continue }
+            subscriptionGroupIds.append(groupId)
+        }
+
+        for groupId in subscriptionGroupIds {
+            let renewalStates = try await getSubscriptionRenewalState(groupID: groupId)
+            for state in renewalStates {
+                switch state {
+                case .subscribed, .inGracePeriod:
+                    return true
+                default:
+                    break
+                }
+            }
+        }
+        
+        return false // サブスクリプションがない、または有効でない場合に false を返す
     }
 }
 
