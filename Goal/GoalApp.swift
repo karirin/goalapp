7f5f22f31c0dcb2579694400da9288fb951c9003
karirin <karirin419@gmail.com>
2023-11-19 11:16:24 +0900

    修正

--- Goal/GoalApp.swift
+++ Goal/GoalApp.swift
@@ -12,8 +12,7 @@ import GoogleMobileAds
 import StoreKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-    @EnvironmentObject var appState: AppState
-//    var appState: AppState?
+    var appState = AppState()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
         if FirebaseApp.app() == nil {
@@ -96,6 +95,7 @@ struct GoalApp: App {
     var body: some Scene {
         WindowGroup {
             if FirebaseApp.app() != nil {
+                let appState = appDelegate.appState
                 if appState.isLoading {
                     // Display a loading view while data is#imageLiteral(resourceName: "simulator_screenshot_54C2BA91-46F1-4CE5-8D01-56B0B783DC15.png") loading
                     ZStack {
